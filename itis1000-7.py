2cd87daf2bebafcd9ea284c91e2a7c07745c9564
1ToWy1 <ramill220033@gmail.com>
2026-08-06 12:16:33 +0300

    Update itis1000-7.py

--- itis1000-7.py
+++ itis1000-7.py
@@ -1,14 +1,24 @@
-while True:
-    try:
-        number = int(input("Введите целое положительное число: "))
-        if number > 0:
-            break
-        else:
+def get_positive_int():
+    while True:
+        try:
+            number = int(input("Введите целое положительное число: "))
+            if number > 0:
+                return number
             print("Ошибка: число должно быть положительным (больше 0). Попробуйте снова.")
-    except ValueError:
-        print("Ошибка: нужно ввести целое число. Попробуйте снова.")
+        except ValueError:
+            print("Ошибка: нужно ввести целое число. Попробуйте снова.")
 
-print(f"Обратный отсчёт от {number} до 0:")
-while number >= 0:
-    print(number)
-    number -= 1
+
+def countdown(start):
+    print(f"Обратный отсчёт от {start} до 0:")
+    for i in range(start, -1, -1):
+        print(i)
+
+
+def main():
+    number = get_positive_int()
+    countdown(number)
+
+
+if __name__ == "__main__":
+    main()
