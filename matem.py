77a3f90ae85e2817540d99de2eede1d0b097153f
1ToWy1 <ramill220033@gmail.com>
2026-08-14 16:27:05 +0300

    Add files via upload

---
+++ matem.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self) -> float:
+        pass
+
+
+class Rectangle(Shape):
+    def __init__(self, width: float, height: float):
+        self.width = width
+        self.height = height
+
+    def area(self) -> float:
+        return self.width * self.height
+
+
+class Circle(Shape):
+    def __init__(self, radius: float):
+        self.radius = radius
+
+    def area(self) -> float:
+        return math.pi * (self.radius ** 2)
+
+
+if __name__ == "__main__":
+    shapes = [
+        Rectangle(5, 10),
+        Circle(7)
+    ]
+
+    for shape in shapes:
+        print(f"Площадь фигуры ({shape.__class__.__name__}): {shape.area():.2f}")
