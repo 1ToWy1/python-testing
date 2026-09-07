dd4fe2d169ba1c52769213367c39824ba110e93e
1ToWy1 <ramill220033@gmail.com>
2026-09-07 19:08:54 +0300

    Update xpathh.py

--- xpathh.py
+++ xpathh.py
@@ -1,5 +1,3 @@
-import time
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
@@ -17,3 +15,9 @@ driver.set_window_size(1920, 1080)
 # Поиск элемента по XPath и ввод логина
 user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
 user_name.send_keys("standard_user")
+
+password_input = driver.find_element(By.XPATH, "//input[@id='password']")
+password_input.send_keys("secret_sauce")
+
+login_button = driver.find_element(By.XPATH, "//input[@id='login-button']")
+login_button.click()
