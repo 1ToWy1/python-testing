a4040d5b023b4ec0ff8a6f162e59fc94cd30f175
1ToWy1 <ramill220033@gmail.com>
2026-09-09 16:14:39 +0300

    Update lastofas.py

--- lastofas.py
+++ lastofas.py
@@ -6,7 +6,17 @@ import time
 
 # Настройка параметров браузера Chrome
 options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)  # Чтобы браузер не закрывался автоматически после выполнения
+
+# 1. Отключение менеджмента и проверки утечки паролей (PasswordLeakDetectionEnabled = False)
+prefs = {
+    "profile.password_manager_leak_detection": False,  # Отключает проверку утечки паролей
+    "credentials_enable_service": False,               # Отключает всплывающее окно сохранения паролей
+    "profile.password_manager_enabled": False          # Отключает встроенный менеджер паролей
+}
+options.add_experimental_option("prefs", prefs)
+
+# Чтобы браузер не закрывался автоматически после выполнения
+options.add_experimental_option("detach", True)
 
 # Инициализация веб-драйвера
 driver = webdriver.Chrome(
@@ -21,7 +31,7 @@ driver.get(base_url)
 # Настройка размера окна браузера
 driver.set_window_size(1920, 1080)
 
-# 1. Авторизация на сайте
+# 2. Авторизация на сайте
 user_name_input = driver.find_element(By.ID, "user-name")
 user_name_input.send_keys("standard_user")
 
@@ -34,23 +44,23 @@ login_button.click()
 # Небольшая пауза для завершения загрузки страницы
 time.sleep(2)
 
-# 2. Открытие скрытого бокового меню (бургер-меню)
+# 3. Открытие скрытого бокового меню (бургер-меню)
 burger_menu_button = driver.find_element(By.ID, "react-burger-menu-btn")
 burger_menu_button.click()
 print("Скрытое меню успешно открыто")
 
 # Пауза для завершения анимации открытия меню
-time.sleep(3)
+time.sleep(1)
 
-# 3. Нажатие на кнопку выхода (Logout)
+# 4. Нажатие на кнопку выхода (Logout)
 logout_link = driver.find_element(By.ID, "logout_sidebar_link")
 logout_link.click()
 print("Нажата кнопка Logout")
 
 # Пауза для завершения редиректа на страницу входа
-time.sleep(3)
+time.sleep(2)
 
-# 4. Проверка разлогина путем сравнения текущего URL с базовым
+# 5. Проверка разлогина путем сравнения текущего URL с базовым
 current_url = driver.current_url
 assert current_url == base_url, f"Ошибка: Ожидался URL '{base_url}', но получен '{current_url}'"
 print(f"Проверка успешна: пользователь разлогинен, текущий URL: {current_url}")
